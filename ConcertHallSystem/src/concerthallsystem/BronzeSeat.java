00d6daaf25bb68566ffb2083fdc634ee1fb06b5f
Daniel <danielcrblack@gmail.com>
2017-02-14 17:13:03 +0000

    Removed unnecessary compareTo

--- ConcertHallSystem/src/concerthallsystem/BronzeSeat.java
+++ ConcertHallSystem/src/concerthallsystem/BronzeSeat.java
@@ -1,15 +1,10 @@
 package concerthallsystem;
 
-public class BronzeSeat extends Seat implements Comparable 
+public class BronzeSeat extends Seat
 {   
     public BronzeSeat(String row, int num, int index)
     {
         super(row, num, index);
-    }
-
-    @Override
-    public int compareTo(Object t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    }   
 }
 
