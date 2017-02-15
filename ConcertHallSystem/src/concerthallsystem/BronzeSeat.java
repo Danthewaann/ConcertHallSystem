2c91772ed12ba7c382472e878e4581fdaa92d92c
Daniel <danielcrblack@gmail.com>
2017-02-15 10:40:51 +0000

    Implemented CannotUnbookSeatException
    Now when you try to unbook a BronzeSeat, the CannotUnbookSeatException is thrown

--- ConcertHallSystem/src/concerthallsystem/BronzeSeat.java
+++ ConcertHallSystem/src/concerthallsystem/BronzeSeat.java
@@ -6,5 +6,13 @@ public class BronzeSeat extends Seat
     {
         super(row, num, index);
     }   
+
+    @Override
+    public void unBook(Customer customer) throws CannotUnbookSeatException
+    {
+        throw new CannotUnbookSeatException(
+            this.getSeatPosition(), "Reason: seat (" + this.getSeatPosition() + ") is in the Bronze section"
+        );
+    }       
 }
 
