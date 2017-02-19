3b11404b2aec6a3d4ee5e34f2c944b639a457a1f
Daniel <danielcrblack@gmail.com>
2017-02-19 19:12:03 +0000

    New ConcertIOException
    It is thrown when any incorrect information is detected on file when trying to load a concert

---
+++ ConcertHallSystem/src/concerthallsystem/ConcertIOException.java
@@ -0,0 +1,16 @@
+package concerthallsystem;
+
+/**
+ *
+ * @author Daniel
+ */
+
+public class ConcertIOException extends RuntimeException 
+{
+  
+    public ConcertIOException(int lineNum)
+    {
+        super("Failed to load concert on line " + lineNum);
+    }
+    
+}
